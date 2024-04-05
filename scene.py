ef191e15eea86583f5602f6cf07e06afa3226155
joeyhk2020 <83687187+joeyhk2020@users.noreply.github.com>
2024-04-05 12:55:11 -0400

    Add files via upload

--- scene.py
+++ scene.py
@@ -1,114 +1,189 @@
+import serial
+from time import sleep
 from arena import *
-
 from threading import Thread
+import ast
 
-# class DemoObject(object):
-#     def __init__(self):
-#         self.pos = Position(0,0,0)
-#         self.rot = Rotation(0,0,0)
-#         self.obj = Box(object_id="my_box", position=Position(0,4,-2), scale=Scale(2,2,2))
-
-# setup library
 scene = Scene(host="arenaxr.org", scene="mini_demo")
 
-# make a box
-box = Box(object_id="my_box", position=Position(0,4,-2), scale=Scale(2,2,2))
-sword = GLTF(
-        object_id="sword",
-        position=(0,3,0),
-        scale=(0.5,0.5,0.5),
-        url="store/users/joeym/sword.glb",
-    )
+scale_factors = [0.8, 1.2, 0.4, 0.4]
 
-ramen = GLTF(
-        object_id="ramen",
-        position=(0,1,0),
-        scale=(10,10,10),
-        url="store/users/joeym/Ramen.gltf",
-    )
+box = Box(object_id="box", 
+          position=Position(0,0,0), 
+          scale=Scale(0.001,0.001,0.001),
+          persist=True)
+parrot = GLTF(object_id="parrot", 
+                position=Position(0,0,0), 
+                scale=Scale(0.001,0.001,0.001),
+                url="store/users/johnchoi/PolyCam/PolyCam_Parrot_Photogrammetry.glb",
+                persist=True)
+xr_logo = GLTF(
+        object_id="xr-logo",
+        position=(0,-0.1,0),
+        scale=(0.001,0.001,0.001),
+        url="store/users/wiselab/models/XR-logo.glb",
+        persist=True)
+shine = GLTF(
+        object_id="shine",
+        position=(0,0,0),
+        scale=(0.001,0.001,0.001),
+        url="store/users/joeym/Shine.gltf",
+        persist=True)
+keys = []
 
-# ball = Sphere(object_id="ball", position=Position(0,4,0), scale=Scale(2,2,2))
+keys_clicked = []
 
-# demo_objects = [sword, ball]
+mouse_keys_down = [0] * 16
+
+for i in range(16):
+    name = "key" + str(i)
+    x = i%4
+    y = i//4
+    xpos = 0.3 + (x*0.08)
+    zpos = 0 + (0.08*y)
+    ypos = 0
+
+    pos = Position(xpos, ypos, zpos)
+    sca = Scale(0.04,0.04,0.04)
+    col = Color(50,50,50)
+    key = Box(object_id=name, position=pos, scale=sca, color=col, persist=True)
+    keys.append(key)
+
+
+interactives = [box, parrot, xr_logo, shine]
+
+obj_sel = 0
+obj_sel_prev = 0
 
 @scene.run_once
 def main():
+    global box
+
+    for key in keys:
+        scene.update_object(key, click_listener=True, evt_handler=mouse_handler)
+
+    scene.add_objects([box, parrot, xr_logo, shine])
+    scene.add_objects(keys)
+
+
+def update_keys(keypad_keys_down):
+    global keys
+    global mouse_keys_down
+    # if (type(keypad_keys_down) is not list) and 1:
+    #     return
+
+    
+    for i in range(16):
+        if (i in keypad_keys_down) or (mouse_keys_down[i] > 0):
+            div = i//4
+            if div == 0:
+                keys[i].update_attributes(color = Color(200,0,0))
+            elif div == 1:
+                keys[i].update_attributes(color = Color(0,200,0))
+            elif div == 2:
+                keys[i].update_attributes(color = Color(0,0,200))
+            else:
+                keys[i].update_attributes(color = Color(200,200,0))
+        else:
+            keys[i].update_attributes(color = Color(50,50,50))
+    
+    scene.update_objects(keys)
+
+def update_scene(data_arr):
     global scene
-    global sword
-    global ramen
-    # add the box
+    global interactives
+    global obj_sel
+    global obj_sel_prev
+    print(obj_sel)
+
+
+    val0 = 0
+    val1 = 0
+    keypad_keys_down = []
+    try:
+        val0 = float(data_arr[0][2:])
+        val1 = float(data_arr[1])
+        keypad_keys_down = ast.literal_eval(data_arr[2][:-1])
+
+        if val0 > 3.35 or val1 > 3.35:
+            return
+    except:
+        return
+
+    # print(val0, val1)
+    try:
+        for i in range(len(interactives)):
+            if i in keypad_keys_down:
+                obj_sel = i
+    except:
+        pass
+
+
+    if obj_sel_prev != obj_sel:
+        obj_sel_prev = obj_sel
+        for i in range(len(interactives)):
+            if i != obj_sel:
+                interactives[i].update_attributes(scale=Scale(0.001,0.001,0.001))
+                scene.update_object(interactives[i])
     
-    current_sel = 0
-    scene.add_object(ramen)
+    update_keys(keypad_keys_down)
 
-    # Slider0 = 0
-    # Slider1 = 0
-    # Slider2 = 0
-    
-    # while True:
-    #     val = input("Enter Value: ")
-    #     vals_list = val.split()
-    #     if vals_list[0] == "A":
-    #         num = int(vals_list[1])
-    #         Slider0 = num
-    #     if vals_list[0] == "B":
-    #         num = int(vals_list[1])
-    #         Slider1 = num
-    #     if vals_list[0] == "C":
-    #         num = int(vals_list[1])
-    #         Slider2 = num
+
+    ob = interactives[obj_sel]
+    roty = val0 * 360/3.3
+    obj_scale = (val1 * 0.15/3.3 + 0.15) * scale_factors[obj_sel]
+
+    ob.update_attributes(rotation=Rotation(0,roty,0), scale=Scale(obj_scale,obj_scale,obj_scale))
+    scene.update_object(ob)
+
+def mouse_handler(scene, evt, msg):
+    global mouse_keys_down
+    if evt.type == "mousedown":
+        name = msg["object_id"]
+        try:
+            num = int(name[3:])
+            mouse_keys_down[num] += 1
+        except:
+            pass
+        # print(mouse_keys_down)
+    elif evt.type == "mouseup":
+        name = msg["object_id"]
+        try:
+            num = int(name[3:])
+            mouse_keys_down[num] -= 1
+        except:
+            pass
+
+def get_mouse_keys_down():
+    global mouse_keys_down
+    down = []
+    for i in range(16):
+        if mouse_keys_down[i] > 0:
+            val = ""
+            if i < 9:
+                val = "0" + str(i)
+            else:
+                val = str(i)
+
+            down.append(val)
+    return down
+
+def t():
+    global box
+    ser = serial.Serial("/dev/ttyS0", 9600)
+    ser.write("START".encode('utf-8'))
+    while True:
+        rd = ser.read()
+        sleep(0.08)
+        dl = ser.inWaiting()
+        rd += ser.read(dl)
+        data_arr = str(rd).split(":")
+        update_scene(data_arr)
+        ser.write(str(get_mouse_keys_down()).encode('utf-8'))
+
         
-        # if vals_list[0] == "Choose":
-        #     sel = int(vals_list[1])
-        #     if sel == 0:
-        #         scene.delete_object(ball)
-        #         scene.add_object(sword)
-        #     else:
-        #         scene.delete_object(sword)
-        #         scene.add_object(ball)
-
-        
-        # current_obj.update_attributes(rotation=Rotation(Slider0, Slider1, Slider2))
-        # scene.update_object(current_obj)
 
 
-# x = 0
-# @scene.run_forever(interval_ms=500)
-# def periodic():
-#     global x    # non allocated variables need to be global
-#     box.update_attributes(position=Position(x,3,0))
-#     scene.update_object(box)
-#     x += 0.1
-
-# start tasks
-
-# def keyboard_inputs():
-    # Slider0 = 0
-    # Slider1 = 0
-    # Slider2 = 0
-    # y = 0
-    # global scene
-    # global sword
-    # while True:
-    #     val = input("Enter Value: ")
-    #     vals_list = val.split()
-    #     if vals_list[0] == "A":
-    #         num = int(vals_list[1])
-    #         Slider0 = num
-    #     if vals_list[0] == "B":
-    #         num = int(vals_list[1])
-    #         Slider1 = num
-    #     if vals_list[0] == "C":
-    #         num = int(vals_list[1])
-    #         Slider2 = num
-        
-    #     if vals_list[0] == "Choose":
-    #         sel = int(vals_list[1])
-        
-    #     sword.update_attributes(rotation=Rotation(Slider0, Slider1, Slider2))
-    #     scene.update_object(sword)
-
-
-# t1 = Thread(target=keyboard_inputs)
-# t1.start()
+t1 = Thread(target=t)
+t1.start()
 scene.run_tasks()
