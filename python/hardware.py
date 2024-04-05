00c49758b7554d726d4e5c6bbc0c3571b5eb50ef
Joey Mok <joeymok@joeys-mbp.wifi.local.cmu.edu>
2024-04-05 12:59:54 -0400

    Version 1 Completed

--- python/hardware.py
+++ python/hardware.py
@@ -7,4 +7,3 @@ while True:
     data_left = ser.inWaiting()             #check for remaining byte
     received_data += ser.read(data_left)
     print (received_data)                   #print received data
-    ser.write(received_data)                #transmit data serially 
